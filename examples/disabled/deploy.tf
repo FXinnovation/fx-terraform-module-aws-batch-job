edd02fb3b1c1dded2c349684b26ceaa7bab1bac5
Julien Cabillot <julien.cabillot@fxinnovation.com>
2021-03-01 10:04:57 -0500

    fix: remove disabled

--- examples/disabled/deploy.tf
+++
@@ -1,9 +0,0 @@
-module "disabled" {
-  source = "../../"
-
-  count = 0
-
-  name          = "tftest"
-  properties    = "tftest"
-  job_queue_arn = "arn:aws:batch:ca-central-1:000000000000:job-queue/tftest"
-}
